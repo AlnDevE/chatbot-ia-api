43390195276fc43450761b50a34084deaa94c928
Alan <alanfernando2809@gmail.com>
2023-03-13 20:00:04 -0300

    create logic to create model

---
+++ train.py
@@ -0,0 +1,75 @@
+import json
+import pickle
+import random
+import nltk
+import numpy as np
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.optimizers import SGD
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
+
+def train_ia():
+    
+    objetives_file = open('objectives.json').read()
+    objectives = json.loads(objetives_file)
+
+    words = []
+    classes = []
+    documents = []
+    ignore_letters = ['!', '?', ',', '.']
+
+    for objective in objectives['objectives']:
+        for pattern in objective['patterns']:
+            word = nltk.word_tokenize(pattern)
+            words.extend(word)
+            documents.append((word, objective['tag']))
+            if objective['tag'] not in classes:
+                classes.append(objective['tag'])
+
+    words = [lemmatizer.lemmatize(w.lower())
+            for w in words if w not in ignore_letters]
+    words = sorted(list(set(words)))
+    classes = sorted(list(set(classes)))
+
+    pickle.dump(words, open('all_words.pkl', 'wb'))
+    pickle.dump(classes, open('all_classes.pkl', 'wb'))
+
+    training = []
+    output_empty = [0] * len(classes)
+    for doc in documents:
+        bag = []
+        pattern_words = doc[0]
+        pattern_words = [lemmatizer.lemmatize(
+            word.lower()) for word in pattern_words]
+        for word in words:
+            bag.append(1) if word in pattern_words else bag.append(0)
+
+        output_row = list(output_empty)
+        output_row[classes.index(doc[1])] = 1
+
+        training.append([bag, output_row])
+        
+    random.shuffle(training)
+    training = np.array(training)
+    train_x = list(training[:, 0])
+    train_y = list(training[:, 1])
+
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_y[0]), activation='softmax'))
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy',
+                optimizer=sgd, metrics=['accuracy'])
+
+    hist = model.fit(np.array(train_x), np.array(train_y),
+                    epochs=200, batch_size=5, verbose=1)
+    model.save('chat_fatec_model.h5', hist)
